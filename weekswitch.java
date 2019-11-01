8ab12f7e288803048717485725b21f6a9ecb1564
unknown <it.mohit07@yahoo.in>
2019-11-01 13:24:13 +0530

    weekswitch.java

---
+++ weekswitch.java
@@ -0,0 +1,48 @@
+public class  weekswitch
+{
+public static void main(String agrs[])
+
+{
+int week =4;
+String  weeknumber;
+switch(week)
+{
+case 1:
+weeknumber="Monday";
+break;
+case 2:
+	weeknumber="Tuseday";
+break;
+case 3:
+	weeknumber="Wednesday";
+break;
+case 4:
+	weeknumber="Thrusday";
+break;
+case 5:
+	weeknumber="Friday";
+break;
+case 6:
+	weeknumber="Saturday";
+break;
+case 7:
+	weeknumber="Sunday";
+break;
+default:
+	weeknumber="invalid";
+}
+System.out.print(weeknumber);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
